62b20028b4fa1d4b5596644ce8b782a42cacac75
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-12-08 13:56:50 +0000

    設定

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,10 +1,37 @@
-class Public::SessionsController < ApplicationController
-  def new
-  end
+# frozen_string_literal: true
 
-  def create
-  end
+class Public::SessionsController < Devise::SessionsController
+   before_action :customer_state, only: [:create]
 
-  def destroy
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    # 入力されたemailからアカウントを一件取得
+    @customer = Customer.find_by(email: params[:customer][:password])
+    # アカウントを取得出来なかった場合、このメソッドを終了する
+    return if !@customer
+    # 取得したアカウントのパスワードと入力されたパスワードが一致しているかを判断
+    if @customer.valid_password?(params[:customer][:password])
+    # 条件式の記述が
   end
+    
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,85 +1,37 @@
 Rails.application.routes.draw do
-  # 顧客用
+  
+# 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip:[:passwords], controllers: {
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip:[:registrations,:passwords],controllers: {
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+  
   namespace :admin do
+    root to:"homes#top"
     get 'order_details/update'
+    resources :orders, only:[:show, :update]
+    resources :customers, only:[:index, :create, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
   end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/create'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
+  
+  
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+    resources :orders, only:[:new, :create, :index, :show]
     get 'orders/comfirm'
     get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    resources :customers, only:[:show, :edit, :update]
     get 'customers/unsubscribe'
     get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
+    resources :items, only:[:index, :show]
     get 'homes/top'
     get 'homes/about'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
