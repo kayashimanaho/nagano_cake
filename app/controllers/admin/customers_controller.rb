2f98a68969329f8c314396aae9bda75ab1575ae1
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-12-16 06:22:56 +0000

    12/16

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,4 +16,11 @@ class Admin::CustomersController < ApplicationController
     @customer.update(customer_params)
      redirect_to admin_customer_path(@customer.id)
   end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :reset_password_token, :reset_password_sent_at, :remember_created_at,:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+  end
 end
+
