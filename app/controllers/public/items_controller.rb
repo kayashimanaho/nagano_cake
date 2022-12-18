4230ef881f370f7949f94b7fe356c1f64fc30c25
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2022-12-18 15:30:32 +0000

    会員商品一覧

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,10 @@
 class Public::ItemsController < ApplicationController
    before_action :authenticate_customer!, except: [:index]
   def index
+    @item = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
